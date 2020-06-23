b3e3b84a972915cfa079988a2952bd9943123e49
Douglas Soares <sdouglas5107@gmail.com>
2020-06-22 22:35:14 -0300

    update start script

--- src/core/database/config/index.js
+++ src/core/database/config/index.js
@@ -1,6 +1,7 @@
+console.log('DATABASE_URL',process.env.DATABASE_URL)
 module.exports = {
   client: "pg",
-  connection: process.env.POSTGRES_URL,
+  connection: process.env.DATABASE_URL,
   pool: { min: 0, max: 7 },
   migrations: {
     tableName: "migrations",
