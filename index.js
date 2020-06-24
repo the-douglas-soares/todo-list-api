36bc6b7fca36d641ac1abaddd3285f10100da563
Douglas Soares <sdouglas5107@gmail.com>
2020-06-24 20:35:50 -0300

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./src/core/routes");
 const morgan = require("morgan");
+const cors = require('cors');
 
 const server = express();
-
+server.use(cors())
 server.use(bodyParser.json());
 server.use(morgan("tiny"));
 server.use(routes);
